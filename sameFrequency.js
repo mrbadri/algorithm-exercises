9f0a1eebe99b32b77e09f4d9b61692b81074f6dc
mrbadri <mrbadri.dev@gmail.com>
2024-08-05 23:37:27 +0330

    feat: first solution for sameFrequency method

--- sameFrequency.js
+++ sameFrequency.js
@@ -11,6 +11,26 @@ sameFrequency (3589578, 5879385) // true
 sameFrequency (22,222) // false
 **/
 
-function sameFrequency(){
+function sameFrequency(num1, num2) {
+  const numsrt1 = num1.toString();
+  const numsrt2 = num2.toString();
+
+  if (numsrt1.length !== numsrt2.length) return false;
+
+  for (let i = 0; i < numsrt1.length; i++) {
+    let hasFrq = false;
+    for (let j = 0; j < numsrt2.length; j++) {
+      if (numsrt1[i] === numsrt2[j]) hasFrq = true;
+    }
+    if (!hasFrq) return false;
+  }
+
+  return true;
+
   // good luck. Add any arguments you deem necessary.
 }
+
+console.log(sameFrequency(182, 281)); // true
+console.log(sameFrequency(34, 14)); // false
+console.log(sameFrequency(3589578, 5879385)); // true
+console.log(sameFrequency(22, 222)); // false
