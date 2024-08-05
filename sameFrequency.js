f791b2794e72dfe0ffd795b8d25b4e9bf979d834
mrbadri <mrbadri.dev@gmail.com>
2024-08-06 00:02:14 +0330

    feat: second solution for sameFrequency method

--- sameFrequency.js
+++ sameFrequency.js
@@ -12,25 +12,33 @@ sameFrequency (22,222) // false
 **/
 
 function sameFrequency(num1, num2) {
-  const numsrt1 = num1.toString();
-  const numsrt2 = num2.toString();
+  const numstr1 = num1.toString();
+  const numstr2 = num2.toString();
 
-  if (numsrt1.length !== numsrt2.length) return false;
+  if (numstr1.length !== numstr2.length) return false;
 
-  for (let i = 0; i < numsrt1.length; i++) {
-    let hasFrq = false;
-    for (let j = 0; j < numsrt2.length; j++) {
-      if (numsrt1[i] === numsrt2[j]) hasFrq = true;
-    }
-    if (!hasFrq) return false;
+  const counter1 = {};
+  const counter2 = {};
+
+  for (let i = 0; i < numstr1.length; i++) {
+    counter1[numstr1[i]] = (counter1[numstr1[i]] || 0) + 1;
+  }
+
+  for (let i = 0; i < numstr2.length; i++) {
+    counter2[numstr2[i]] = (counter2[numstr2[i]] || 0) + 1;
+  }
+
+  // console.log(counter1);
+
+  for (let key in counter1) {
+    if (counter1[key] !== counter2[key]) return false;
   }
 
   return true;
-
   // good luck. Add any arguments you deem necessary.
 }
 
 console.log(sameFrequency(182, 281)); // true
 console.log(sameFrequency(34, 14)); // false
 console.log(sameFrequency(3589578, 5879385)); // true
-console.log(sameFrequency(22, 222)); // false
+console.log(sameFrequency(22, 222)); // true
