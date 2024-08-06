8f4ee77a4db259dd8e53a2d482a4a6792ed7fe99
unknown <mrbadri.dev@gmail.com>
2024-08-06 12:39:13 -0700

    feat: add shift and unshift method to SimglyLinkList

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -46,9 +46,34 @@ class SinglyLinkedList{
         return current;
     }
     shift(){
+        if(!this.head) return undefined;
+
+        const temp = this.head;
+        this.head = temp.next
+        
+        this.length--;
+
+        if(this.length === 0){
+            this.tail = null;
+        }
+        
+        return temp;
         // code here
     }
     unshift(val){
+        const newNode = new Node(val);
+
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+
+        return this;
        // code here
     }
     get(index){
