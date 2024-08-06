8ef9d3efd623761b69c33e0ff6c47d8f93ddb895
unknown <mrbadri.dev@gmail.com>
2024-08-06 12:05:33 -0700

    feat: add dequeue method for Queue

--- queue.js
+++ queue.js
@@ -2,34 +2,39 @@
   Please add dequeue function to the following Queue 
 **/
 
-
-
 class Node {
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class Queue {
-    constructor(){
-        this.first = null;
-        this.last = null;
-        this.size = 0;
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
     }
-    enqueue(val){
-        var newNode = new Node(val);
-        if(!this.first){
-            this.first = newNode;
-            this.last = newNode;
-        } else {
-            this.last.next = newNode;
-            this.last = newNode;
-        }
-        return ++this.size;
-    }
+    return ++this.size;
+  }
 
-    dequeue(){
-        // return null or a value
-    }
+  dequeue() {
+    // return null or a value
+    if (!this.first) return null;
+    const temp = this.first;
+    if (this.first === this.last) this.last = null;
+    this.first = this.first?.next;
+    this.size--;
+    
+    return temp.value;
+  }
 }
